5b84a3d4fcefba4936a094679dd6ba931550e4e5
Frank Morgner <morgnerf@ub.uni-leipzig.de>
2024-10-30 13:02:51 +0100

    refs #26930 fixes config variables by removing not longer used subparameter value * deletes support for typo3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'RSS News Feed',
 	'description' => 'This extension imports rss news items from external rss feeds. This extension builds using extbase and fluid. It does not import anything into the database and it only displays the rss news articles in front end.',
 	'category' => 'plugin',
-	'version' => '2.1.3',
+	'version' => '2.1.4',
 	'state' => 'stable',
 	'uploadfolder' => false,
 	'createDirs' => '',
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = [
 	[
 		'depends' => 
 		[
-			'typo3' => '8.0.0 - 9.5.99',
+			'typo3' => '9.0.0 - 9.5.99',
 		],
 		'conflicts' => 
 		[
